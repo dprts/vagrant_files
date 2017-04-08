a9437dce6283a4beff247fea4279df50d82639f1
Piotr Olczak <piotr.olczak@gmail.com>
2017-04-09 00:24:47 +0200

    cleanup + vault

--- docker-dc/gluster.sh
+++ docker-dc/gluster.sh
@@ -2,7 +2,53 @@
 
 set -o errexit
 
-if [ "$(uname -n)" == "manager1" ]; then
+run_consul_server() {
+  docker run -d \
+    --name=consul-server-at-$(uname -n) \
+    --hostname=$(uname -n) \
+    -e 'CONSUL_LOCAL_CONFIG={"skip_leave_on_interrupt": true, "datacenter": "testDC", "acl_master_token": "95790f9c-3e98-46f5-967e-8f3ffb02194c", "acl_datacenter": "TestDC", "acl_default_policy": "allow", "acl_down_policy": "allow" }' \
+    --net=host \
+    -v /srv/consul/data:/consul/data \
+    consul:0.7.5 agent -server -ui -bind=$(ip a s dev eth1 | awk '/inet / {split($2,a,"/"); print a[1]}') \
+    -client 0.0.0.0 \
+    -bootstrap-expect 3 \
+    -retry-join 172.20.21.11 \
+    -retry-join 172.20.21.12 \
+    -retry-join 172.20.21.13 \
+    -retry-interval 5s
+}
+
+run_consul_agent() {
+  docker run -d \
+    --name=consul-agent-at-$(uname -n) \
+    --hostname=$(uname -n) \
+    --net=host \
+    -v /srv/consul/data:/consul/data \
+    -e 'CONSUL_LOCAL_CONFIG={"datacenter": "testDC"}' \
+    consul:0.7.5 agent -ui \
+    -bind=$(ip a s dev eth1 | awk '/inet / {split($2,a,"/"); print a[1]}') \
+    -client 0.0.0.0
+
+  for i in `seq 1 10`; do
+    sleep 1
+    echo -n "."
+  done
+
+  docker exec consul-agent-at-$(uname -n) consul join 172.20.21.11
+}
+
+run_vault() {
+  NODE_IP=$(ip a s dev eth1 | awk '/inet / {split($2,a,"/"); print a[1]}')
+  docker run -d \
+    --name=vault-at-$(uname -n) \
+    --hostname=$(uname -n) \
+    --net=host \
+    --cap-add=IPC_LOCK \
+    -e "VAULT_LOCAL_CONFIG={\"backend\": {\"consul\": { \"address\": \"${NODE_IP}:8500\", \"path\": \"vault\" }}, \"listener\": {\"tcp\": { \"address\": \"0.0.0.0:8200\", \"tls_disable\": \"1\"}}, \"default_lease_ttl\": \"168h\", \"max_lease_ttl\": \"720h\"}" \
+    vault server
+}
+
+setup_glusterd() {
   set +o errexit
   gluster volume info glustervol1
   retval=$?
@@ -20,18 +66,19 @@ if [ "$(uname -n)" == "manager1" ]; then
     gluster volume start glustervol1
     gluster volume info all
   fi
-fi
+}
 
-case $(uname -n) in
-  'manager1' | 'manager2' | 'manager3')
+mount_glustervol() {
     mkdir -p /srv/portainer || :
     mount -t glusterfs $(uname -n):/glustervol1 /srv/portainer || :
     grep -q portainer /etc/fstab || echo "$(uname -n):/glustervol1   /srv/portainer    glusterfs defaults 0 0" >> /etc/fstab
-    ;;
-esac
+}
 
-if [ "$(uname -n)" == "manager1" ]; then
+init_swarm() {
   docker swarm init --advertise-addr $(ip a l dev eth1 | awk '/inet / { split($2, a, "/"); print a[1]}')
+}
+
+run_portainer_as_svc() {
   docker service create --name portainer \
     --publish 9000:9000 \
     --constraint 'node.role == manager' \
@@ -39,13 +86,15 @@ if [ "$(uname -n)" == "manager1" ]; then
     --mount type=bind,src=/srv/portainer,dst=/data \
     portainer/portainer \
       -H unix:///var/run/docker.sock
+}
 
+run_consul_as_svc() {
   docker network create --driver overlay --subnet 172.21.0.0/24 consul
 
   docker service create \
     --network=consul \
     --name=consul \
-    -e 'CONSUL_LOCAL_CONFIG={"skip_leave_on_interrupt": true, "datacenter": "testDC"}' \
+    -e 'CONSUL_LOCAL_CONFIG={"skip_leave_on_interrupt": true, "datacenter": "testDC", "acl_master_token": "95790f9c-3e98-46f5-967e-8f3ffb02194c", "acl_datacenter": "TestDC", "acl_default_policy": "allow", "acl_down_policy": "allow"}' \
     -e 'CONSUL_BIND_INTERFACE=eth0' \
     --mode global \
     -p 8300-8302:8300-8302 \
@@ -63,5 +112,55 @@ if [ "$(uname -n)" == "manager1" ]; then
     -retry-join 172.21.0.4 \
     -retry-join 172.21.0.5 \
     -retry-interval 5s
+}
+exec_join_workers() {
+   WORKER_TOKEN=$(docker swarm join-token -q worker)
+   NODE_IP=$(ip a s dev eth1 | awk '/inet / {split($2,a,"/"); print a[1]}')
+   for n in manager{2,3} worker{1,2}; do
+     ssh \
+       -i /vagrant/.vagrant/machines/${n}/libvirt/private_key \
+       -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no \
+       vagrant@$n \
+       sudo docker swarm join --token $WORKER_TOKEN ${NODE_IP}:2377
+   done
+}
 
-fi
+exec_promote_managers() {
+   for n in manager{2,3}; do
+     docker node promote $n
+   done
+}
+exec_gluster_sh() {
+   for n in manager{2,3} worker{1,2}; do
+     ssh \
+       -i /vagrant/.vagrant/machines/${n}/libvirt/private_key \
+       -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no \
+       vagrant@$n \
+       sudo /vagrant/gluster.sh
+   done
+}
+
+
+### Main ###
+
+case $(uname -n) in
+  'manager1')
+    setup_glusterd
+    mount_glustervol
+    init_swarm
+    run_portainer_as_svc
+    run_consul_server
+    exec_join_workers
+    exec_promote_managers
+    exec_gluster_sh
+    ;;
+  'manager2' | 'manager3')
+    mount_glustervol
+    run_consul_server
+    ;;
+  'worker1' | 'worker2')
+    run_consul_agent
+    ;;
+esac
+
+run_vault
